d0db1811e86b8fbd539cd776e92f1b32cfa48e46
joanna <jo.senczuk@gmail.com>
2017-11-14 14:24:06 +0000

    Update BookUpdateValidator

--- src/main/java/com/example/library/web/book/BookUpdateValidator.java
+++ src/main/java/com/example/library/web/book/BookUpdateValidator.java
@@ -1,4 +1,34 @@
 package com.example.library.web.book;
 
-public class BookUpdateValidation {
+import com.example.library.domain.book.BookService;
+import com.example.library.web.ErrorsResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class BookUpdateValidator {
+
+    private BookService service;
+
+    public BookUpdateValidator(BookService service) {
+        this.service = service;
+    }
+
+    ErrorsResource validate(BookResource resource) {
+        List<String> validationErrors = new ArrayList<>();
+
+        if (resource.getTitle() == null || resource.getTitle().isEmpty()) {
+            validationErrors.add("Title not specified");
+        }
+        if (resource.getAuthor() == null || resource.getAuthor().isEmpty()) {
+            validationErrors.add("Author not specified");
+        }
+
+        return new ErrorsResource(validationErrors);
+    }
+
+
 }
