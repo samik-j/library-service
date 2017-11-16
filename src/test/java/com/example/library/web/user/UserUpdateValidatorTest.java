a017006f904af87edbb236dddd166f93ce70972d
joanna <jo.senczuk@gmail.com>
2017-11-16 14:58:01 +0000

    Add tests for UserUpdateValidator

---
+++ src/test/java/com/example/library/web/user/UserUpdateValidatorTest.java
@@ -0,0 +1,37 @@
+package com.example.library.web.user;
+
+import com.example.library.web.ErrorsResource;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserUpdateValidatorTest {
+
+    private UserUpdateValidator validator = new UserUpdateValidator();
+
+    @Test
+    public void shouldValidateWithNoErrors() {
+        // given
+        UserResource resource = new UserResource();
+        resource.setLastName("Last");
+
+        // when
+        ErrorsResource result = validator.validate(resource);
+
+        // then
+        assertTrue(result.getValidationErrors().isEmpty());
+    }
+
+    @Test
+    public void shouldValidateWithErrorIfLastNameIsEmptyOrNull() {
+        // given
+        UserResource resource = new UserResource();
+
+        // when
+        ErrorsResource result = validator.validate(resource);
+
+        // then
+        assertTrue(result.getValidationErrors().contains("Last name not specified"));
+    }
+
+}
