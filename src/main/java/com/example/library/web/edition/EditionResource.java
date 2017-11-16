4fc27887ffcfb0c3b781e0a6498faa78c9415486
joanna <jo.senczuk@gmail.com>
2017-11-16 19:25:15 +0000

    Override hasCode() and equals() in EditionResource

--- src/main/java/com/example/library/web/edition/EditionResource.java
+++ src/main/java/com/example/library/web/edition/EditionResource.java
@@ -71,4 +71,44 @@ public class EditionResource {
     public void setBookId(long bookId) {
         this.bookId = bookId;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        EditionResource resource = (EditionResource) o;
+
+        if (id != resource.id) {
+            return false;
+        }
+        if (quantity != resource.quantity) {
+            return false;
+        }
+        if (onLoan != resource.onLoan) {
+            return false;
+        }
+        if (bookId != resource.bookId) {
+            return false;
+        }
+        if (isbn != null ? !isbn.equals(resource.isbn) : resource.isbn != null) {
+            return false;
+        }
+        return publicationYear != null ? publicationYear.equals(resource.publicationYear) : resource.publicationYear == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (isbn != null ? isbn.hashCode() : 0);
+        result = 31 * result + (publicationYear != null ? publicationYear.hashCode() : 0);
+        result = 31 * result + (int) (quantity ^ (quantity >>> 32));
+        result = 31 * result + (int) (onLoan ^ (onLoan >>> 32));
+        result = 31 * result + (int) (bookId ^ (bookId >>> 32));
+        return result;
+    }
 }
